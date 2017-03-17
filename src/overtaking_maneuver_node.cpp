c19d580f3b2a39a52094f7bc9fbddd8fcedcf4b4
ahyou <felix.naser@live.de>
2017-03-17 17:32:14 +0800

    testing ppc integration

--- src/overtaking_maneuver_node.cpp
+++ src/overtaking_maneuver_node.cpp
@@ -1,7 +1,10 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "nav_msgs/Path.h"
+#include "nav_msgs/Odometry.h"
 #include "geometry_msgs/PoseStamped.h"
+#include "geometry_msgs/Pose.h"
+#include "geometry_msgs/Twist.h"
 
 #include <dynamic_reconfigure/server.h>
 #include <overtaking_maneuver/OvertakingManeuverInputsConfig.h>
@@ -10,6 +13,7 @@
 
 std::string node_name = "overtaking_maneuver_node";
 std::string pub_path = "overtaking_path";
+std::string sub_odom_topic = "map"; //"odom";
 std::string odom_frame_id = "catvehicle/odom";
 
 // INPUTS
@@ -24,6 +28,8 @@ double input_max_acc = 3.00; // = normal car in the US
 double input_vel_obstacle = 0;
 // Time step size
 double time_step_size = 0.5;
+// Current pose of ego vehicle
+geometry_msgs::Pose current_pose;
 
 double calculate_total_dis(double input_vel, double input_width,
                            double input_max_acc) {
@@ -60,11 +66,24 @@ void dynamic_config_callback(
   input_max_acc = config.input_max_acc;
 }
 
+void odom_callback(const nav_msgs::Odometry::ConstPtr &odom) {
+  current_pose.position.x = odom->pose.pose.position.x;
+  current_pose.position.y = odom->pose.pose.position.y;
+  current_pose.position.z = odom->pose.pose.position.z;
+
+  current_pose.orientation.x = odom->pose.pose.orientation.x;
+  current_pose.orientation.y = odom->pose.pose.orientation.y;
+  current_pose.orientation.z = odom->pose.pose.orientation.z;
+  current_pose.orientation.w = odom->pose.pose.orientation.w;
+}
+
 int main(int argc, char **argv) {
   ros::init(argc, argv, node_name);
 
   ros::NodeHandle n;
   ros::Publisher pub_trajectory = n.advertise<nav_msgs::Path>(pub_path, 1000);
+  ros::Subscriber sub_odom =
+      n.subscribe<nav_msgs::Odometry>(sub_odom_topic, 1000, odom_callback);
   ros::Rate loop_rate(10);
 
   dynamic_reconfigure::Server<
@@ -111,14 +130,14 @@ int main(int argc, char **argv) {
       double x = calculate_x_at_t(input_vel, total_dis, total_time, time);
       double y = calculate_y_at_t(input_width, total_time, time);
 
-      pose_tmp.pose.position.x = x - total_dis;
-      pose_tmp.pose.position.y = (-1) * y + y_t_0;
-      pose_tmp.pose.position.z = 0;
+      pose_tmp.pose.position.x = (x - total_dis) + current_pose.position.x;
+      pose_tmp.pose.position.y = ((-1) * y + y_t_0) + current_pose.position.y;
+      pose_tmp.pose.position.z = (0) + current_pose.position.z;
 
-      pose_tmp.pose.orientation.x = 0;
-      pose_tmp.pose.orientation.y = 0;
-      pose_tmp.pose.orientation.z = 0;
-      pose_tmp.pose.orientation.w = 1;
+      pose_tmp.pose.orientation.x = (0) + current_pose.orientation.x;
+      pose_tmp.pose.orientation.y = (0) + current_pose.orientation.y;
+      pose_tmp.pose.orientation.z = (0) + current_pose.orientation.z;
+      pose_tmp.pose.orientation.w = (1) + current_pose.orientation.w;
 
       path_tmp.poses.push_back(pose_tmp);
 
@@ -130,14 +149,14 @@ int main(int argc, char **argv) {
       double x = calculate_x_at_t(input_vel, total_dis, total_time, time);
       double y = calculate_y_at_t(input_width, total_time, time);
 
-      pose_tmp.pose.position.x = x;
-      pose_tmp.pose.position.y = y;
-      pose_tmp.pose.position.z = 0;
+      pose_tmp.pose.position.x = (x) + current_pose.position.x;
+      pose_tmp.pose.position.y = (y) + current_pose.position.y;
+      pose_tmp.pose.position.z = (0) + current_pose.position.z;
 
-      pose_tmp.pose.orientation.x = 0;
-      pose_tmp.pose.orientation.y = 0;
-      pose_tmp.pose.orientation.z = 0;
-      pose_tmp.pose.orientation.w = 1;
+      pose_tmp.pose.orientation.x = (0) + current_pose.orientation.x;
+      pose_tmp.pose.orientation.y = (0) + current_pose.orientation.y;
+      pose_tmp.pose.orientation.z = (0) + current_pose.orientation.z;
+      pose_tmp.pose.orientation.w = (1) + current_pose.orientation.w;
 
       path_tmp.poses.push_back(pose_tmp);
 
