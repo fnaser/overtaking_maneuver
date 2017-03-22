4e8a54ad118dd96f3afbab01e7ee7a662e689d60
ahyou <felix.naser@live.de>
2017-03-22 16:49:11 +0800

    changed path name and frame ids

--- src/overtaking_maneuver_node.cpp
+++ src/overtaking_maneuver_node.cpp
@@ -1,5 +1,6 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Bool.h"
 #include "nav_msgs/Path.h"
 #include "nav_msgs/Odometry.h"
 #include "geometry_msgs/PoseStamped.h"
@@ -12,9 +13,10 @@
 #include <cmath>
 
 std::string node_name = "overtaking_maneuver_node";
+std::string sub_user_input_topic = "update_odom_user_input";
 std::string pub_path = "overtaking_path";
-std::string sub_odom_topic = "map"; //"odom";
-std::string odom_frame_id = "catvehicle/odom";
+std::string sub_odom_topic = "odom";
+std::string frame_id = "map";
 
 // INPUTS
 // V (>= 5 m/s): Initial and final velocity [m/s]
@@ -30,6 +32,10 @@ double input_vel_obstacle = 0;
 double time_step_size = 0.5;
 // Current pose of ego vehicle
 geometry_msgs::Pose current_pose;
+// Trajectory publisher
+ros::Publisher pub_trajectory;
+// User input
+bool update_odom = true;
 
 double calculate_total_dis(double input_vel, double input_width,
                            double input_max_acc) {
@@ -58,6 +64,96 @@ double calculate_y_at_t(double input_width, double total_time, double time_t) {
                         6 * pow((time_t / total_time), 5));
 }
 
+void user_input_callback(const std_msgs::Bool::ConstPtr &msg) {
+  update_odom = msg->data;
+}
+
+void odom_callback(const nav_msgs::Odometry::ConstPtr &odom) {
+  if (update_odom) {
+    current_pose.position.x = odom->pose.pose.position.x;
+    current_pose.position.y = odom->pose.pose.position.y;
+    current_pose.position.z = odom->pose.pose.position.z;
+
+    current_pose.orientation.x = odom->pose.pose.orientation.x;
+    current_pose.orientation.y = odom->pose.pose.orientation.y;
+    current_pose.orientation.z = odom->pose.pose.orientation.z;
+    current_pose.orientation.w = odom->pose.pose.orientation.w;
+  }
+}
+
+void publish_trajectory() {
+
+  // OUTPUTS
+  // Trajectory path: x(t) and y(t)
+  // D: Total x-direction distance
+  // T: Total time for maneuver
+
+  double total_dis = calculate_total_dis(input_vel, input_width, input_max_acc);
+  double total_time =
+      calculate_total_time(input_vel, input_width, input_max_acc);
+  ROS_INFO("vel %f, width %f, max_acc %f", input_vel, input_width,
+           input_max_acc);
+  ROS_INFO("d %f", total_dis);
+  ROS_INFO("t %f", total_time);
+
+  // init temp pose
+  geometry_msgs::PoseStamped pose_tmp;
+  std_msgs::Header pose_tmp_header;
+  pose_tmp_header.frame_id = "";//frame_id;
+  pose_tmp_header.stamp = ros::Time::now();
+  pose_tmp.header = pose_tmp_header;
+
+  // init temp path
+  nav_msgs::Path path_tmp;
+  std_msgs::Header path_tmp_header;
+  path_tmp_header.frame_id = frame_id;
+  path_tmp_header.stamp = ros::Time::now();
+  path_tmp.header = path_tmp_header;
+
+  double y_t_0 = calculate_y_at_t(input_width, total_time, 0);
+
+  double time = 0;
+  while (time <= total_time) {
+    double x = calculate_x_at_t(input_vel, total_dis, total_time, time);
+    double y = calculate_y_at_t(input_width, total_time, time);
+
+    pose_tmp.pose.position.x = (x - total_dis) + current_pose.position.x;
+    pose_tmp.pose.position.y = ((-1) * y + y_t_0) + current_pose.position.y;
+    pose_tmp.pose.position.z = (0) + current_pose.position.z;
+
+    pose_tmp.pose.orientation.x = (0) + current_pose.orientation.x;
+    pose_tmp.pose.orientation.y = (0) + current_pose.orientation.y;
+    pose_tmp.pose.orientation.z = (0) + current_pose.orientation.z;
+    pose_tmp.pose.orientation.w = (1) + current_pose.orientation.w;
+
+    path_tmp.poses.push_back(pose_tmp);
+
+    time = time + time_step_size;
+  }
+
+  time = 0;
+  while (time <= total_time) {
+    double x = calculate_x_at_t(input_vel, total_dis, total_time, time);
+    double y = calculate_y_at_t(input_width, total_time, time);
+
+    pose_tmp.pose.position.x = (x) + current_pose.position.x;
+    pose_tmp.pose.position.y = (y) + current_pose.position.y;
+    pose_tmp.pose.position.z = (0) + current_pose.position.z;
+
+    pose_tmp.pose.orientation.x = (0) + current_pose.orientation.x;
+    pose_tmp.pose.orientation.y = (0) + current_pose.orientation.y;
+    pose_tmp.pose.orientation.z = (0) + current_pose.orientation.z;
+    pose_tmp.pose.orientation.w = (1) + current_pose.orientation.w;
+
+    path_tmp.poses.push_back(pose_tmp);
+
+    time = time + time_step_size;
+  }
+
+  pub_trajectory.publish(path_tmp);
+  path_tmp.poses.clear();
+}
+
 void dynamic_config_callback(
     overtaking_maneuver::OvertakingManeuverInputsConfig &config,
     uint32_t level) {
@@ -66,25 +162,16 @@ void dynamic_config_callback(
   input_max_acc = config.input_max_acc;
 }
 
-void odom_callback(const nav_msgs::Odometry::ConstPtr &odom) {
-  current_pose.position.x = odom->pose.pose.position.x;
-  current_pose.position.y = odom->pose.pose.position.y;
-  current_pose.position.z = odom->pose.pose.position.z;
-
-  current_pose.orientation.x = odom->pose.pose.orientation.x;
-  current_pose.orientation.y = odom->pose.pose.orientation.y;
-  current_pose.orientation.z = odom->pose.pose.orientation.z;
-  current_pose.orientation.w = odom->pose.pose.orientation.w;
-}
-
 int main(int argc, char **argv) {
   ros::init(argc, argv, node_name);
 
   ros::NodeHandle n;
-  ros::Publisher pub_trajectory = n.advertise<nav_msgs::Path>(pub_path, 1000);
+  pub_trajectory = n.advertise<nav_msgs::Path>(pub_path, 1000);
   ros::Subscriber sub_odom =
       n.subscribe<nav_msgs::Odometry>(sub_odom_topic, 1000, odom_callback);
   ros::Rate loop_rate(10);
+  ros::Subscriber sub_user_input = n.subscribe<std_msgs::Bool>(
+      sub_user_input_topic, 1000, user_input_callback);
 
   dynamic_reconfigure::Server<
       overtaking_maneuver::OvertakingManeuverInputsConfig> server;
@@ -95,79 +182,11 @@ int main(int argc, char **argv) {
 
   while (ros::ok()) {
 
-    // OUTPUTS
-    // Trajectory path: x(t) and y(t)
-    // D: Total x-direction distance
-    // T: Total time for maneuver
-
-    double total_dis =
-        calculate_total_dis(input_vel, input_width, input_max_acc);
-    double total_time =
-        calculate_total_time(input_vel, input_width, input_max_acc);
-    ROS_INFO("vel %f, width %f, max_acc %f", input_vel, input_width,
-             input_max_acc);
-    ROS_INFO("d %f", total_dis);
-    ROS_INFO("t %f", total_time);
-
-    // init temp pose
-    geometry_msgs::PoseStamped pose_tmp;
-    std_msgs::Header pose_tmp_header;
-    pose_tmp_header.frame_id = odom_frame_id;
-    pose_tmp_header.stamp = ros::Time::now();
-    pose_tmp.header = pose_tmp_header;
-
-    // init temp path
-    nav_msgs::Path path_tmp;
-    std_msgs::Header path_tmp_header;
-    path_tmp_header.frame_id = odom_frame_id;
-    path_tmp_header.stamp = ros::Time::now();
-    path_tmp.header = path_tmp_header;
-
-    double y_t_0 = calculate_y_at_t(input_width, total_time, 0);
-
-    double time = 0;
-    while (time <= total_time) {
-      double x = calculate_x_at_t(input_vel, total_dis, total_time, time);
-      double y = calculate_y_at_t(input_width, total_time, time);
-
-      pose_tmp.pose.position.x = (x - total_dis) + current_pose.position.x;
-      pose_tmp.pose.position.y = ((-1) * y + y_t_0) + current_pose.position.y;
-      pose_tmp.pose.position.z = (0) + current_pose.position.z;
-
-      pose_tmp.pose.orientation.x = (0) + current_pose.orientation.x;
-      pose_tmp.pose.orientation.y = (0) + current_pose.orientation.y;
-      pose_tmp.pose.orientation.z = (0) + current_pose.orientation.z;
-      pose_tmp.pose.orientation.w = (1) + current_pose.orientation.w;
-
-      path_tmp.poses.push_back(pose_tmp);
-
-      time = time + time_step_size;
-    }
-
-    time = 0;
-    while (time <= total_time) {
-      double x = calculate_x_at_t(input_vel, total_dis, total_time, time);
-      double y = calculate_y_at_t(input_width, total_time, time);
-
-      pose_tmp.pose.position.x = (x) + current_pose.position.x;
-      pose_tmp.pose.position.y = (y) + current_pose.position.y;
-      pose_tmp.pose.position.z = (0) + current_pose.position.z;
-
-      pose_tmp.pose.orientation.x = (0) + current_pose.orientation.x;
-      pose_tmp.pose.orientation.y = (0) + current_pose.orientation.y;
-      pose_tmp.pose.orientation.z = (0) + current_pose.orientation.z;
-      pose_tmp.pose.orientation.w = (1) + current_pose.orientation.w;
-
-      path_tmp.poses.push_back(pose_tmp);
-
-      time = time + time_step_size;
-    }
-
-    pub_trajectory.publish(path_tmp);
-    path_tmp.poses.clear();
+    publish_trajectory();
 
     ros::spinOnce();
     loop_rate.sleep();
   }
+
   return 0;
 }
